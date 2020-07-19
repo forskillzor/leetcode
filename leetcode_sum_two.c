877915acad5d4ade61239e7fb04c163b21c03b95
skillzor <skillzorwispa@gmail.com>
2020-07-19 17:09:33 +0700

    changing the structure to a simple array

--- leetcode_sum_two.c
+++ leetcode_sum_two.c
@@ -11,44 +11,36 @@ struct item
     int n;
 };
 
-struct item **get_items(int *nums, int size)
+void swap(int *nums, int *indexes, int i, int j)
 {
-    struct item **items = malloc(sizeof(struct item*) * size);
+    int temp_val;
+    int temp_idx;
     
-    for (int i = 0; i < size; ++i)
-    {
-        items[i] = malloc(sizeof(struct item));
-        items[i]->n = nums[i];
-        items[i]->idx = i;
-    }
-    return items;
+    temp_val = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp_val;
+
+    temp_idx = indexes[i];
+    indexes[i] = indexes[j];
+    indexes[j] = temp_idx;
 }
 
-void swap(struct item **items, int i, int j)
-{
-    struct item *temp = NULL;
-
-    temp = items[i];
-    items[i] = items[j];
-    items[j] = temp;
-}
-
-void qsort_(struct item **items, int left, int right)
+void qsort_(int *nums, int *indexes, int left, int right)
 {
     int i, pivot;
-    void swap(struct item **items, int i, int j);
+    void swap(int *nums, int *indexes, int i, int j);
 
     if (left >= right)
         return;
 
-    swap(items, left, (left + right)/2);
+    swap(nums, indexes, left, (left + right)/2);
     pivot = left;
     for (i = left + 1; i <= right; i++)
-        if (items[left]->n > items[i]->n)
-            swap(items, ++pivot, i);
-    swap(items, left, pivot);
-    qsort_(items, left, pivot-1);
-    qsort_(items, pivot + 1, right);
+        if (nums[left] > nums[i])
+            swap(nums, indexes, ++pivot, i);
+    swap(nums, indexes, left, pivot);
+    qsort_(nums, indexes, left, pivot-1);
+    qsort_(nums, indexes, pivot + 1, right);
 }
 
 int binsearch(int* arr, int size, int target)
@@ -75,58 +67,69 @@ int binsearch(int* arr, int size, int target)
         return -1;
 }
 
-void freemem(struct item **items, int size)
+int* get_indexes(int size)
 {
+    int *res = malloc(sizeof(int) * size);
+
     for (int i = 0; i < size; ++i)
-        free(items[i]);
+        res[i] = i;
+        
+    return res;
 }
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     struct item **get_items(int *nums, int size);
-    void qsort_(struct item **items, int left, int right);
+    void qsort_(int *items, int *indexes, int left, int right);
     int binsearch(int* arr, int size, int target);
     void freemem(struct item **items, int size);
+    int* get_indexes(int size);
 
     int t;
     int *res = malloc(sizeof (int)*2);
     int n1, n2;
-    struct item **items = get_items(nums, numsSize);
+    int *indexes = get_indexes(numsSize);
 
-    qsort_(items, 0, numsSize-1);
+    qsort_(nums, indexes, 0, numsSize-1);
 
     int idx1 = 0;
     int idx2 = NOT;
 
-    for (; idx1 < numsSize && items[idx1]->n < target; ++idx1)
+    for (; idx1 < numsSize && nums[idx1] < target; ++idx1)
         ;
 
     --idx1;
 
     do 
     {
-        n1 = items[idx1--]->n;
+        n1 = nums[idx1--];
         t = target - n1;
         if ((idx2 = binsearch(nums, numsSize, t)) == NOT
                                     || (idx1 + 1) == idx2)
                 continue;
 
-        n2 = items[idx2]->n;
+        n2 = nums[idx2];
     }
     while (target - (n1 + n2));
 
-    freemem(items, numsSize);
-    free(items);
-    
-    if (idx2 == NOT)
+
+    if (idx2 == NOT) 
+    {
+        free(indexes);
         return NULL;
+    }
 
     ++idx1;
 
-    res[0] = idx1 < idx2 ? idx1 : idx2;
-    res[1] = idx1 < idx2 ? idx2 : idx1;
+    int i1 = indexes[idx1];
+    int i2 = indexes[idx2];
+
+    res[0] = i1 < i2 ? i1 : i2;
+    res[1] = i1 > i2 ? i1 : i2;
 
     *returnSize = 2;
+
+    free(indexes);
 
     return res;
 }
@@ -134,9 +137,11 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 
 int main(void)
 {
-    int input[] = {2, 4 ,22, 7, 11, 15, 9};
-    int input_length = 7;
-    int target = 19;
+    //int input[] = { 2, 4 ,22, 7, 11, 15, 9 };
+    //int input_length = 7;
+    int input[] = { 3, 2, 4 };
+    int input_length = 3;
+    int target = 6;
     int returnSize = 2;
 
     int *nums = NULL;
@@ -152,8 +157,13 @@ int main(void)
     printf("looking for memory usage...\n");
     getchar();
 #else
-    if ((nums = twoSum(input, input_length, target, &returnSize)) != NULL)
+    if ((nums = twoSum(input, input_length, target, &returnSize)) != NULL) {
+        for (int i = 0; i < input_length; ++i)
+            printf("%d ", input[i]);
+        printf("\n");
+        printf("target: %d\n", target);
         printf("result is: [%d %d]\n", nums[0], nums[1]);
+    }
 #endif
     
     exit(EXIT_SUCCESS);
