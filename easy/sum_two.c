54fc4a3edae6698904fc47bf7c947ef893c8c13d
skillzor <skillzorwispa@gmail.com>
2020-07-20 10:36:46 +0700

    переименовал  фалы и изменил дерево каталога

--- easy/sum_two.c
+++ easy/sum_two.c
@@ -5,8 +5,8 @@
  */
 #define NOT -1
 
-const int input[] = { 23, 99, 100, 2, 10, 9, 17, 0, 8, 11, 4, 3 };
-const int input_length = sizeof(input) / sizeof(input[0]);
+//const int input[] = { 23, 99, 100, 2, 10, 9, 17, 0, 8, 11, 4, 3 };
+//const int input_length = sizeof(input) / sizeof(input[0]);
 
 void swap(int *nums, int *indexes, int i, int j)
 {
@@ -32,7 +32,7 @@ void qsort_(int *nums, int *indexes, int left, int right)
     swap(nums, indexes, left, (left + right)/2);
     pivot = left;
     for (i = left + 1; i <= right; i++)
-        if (nums[left] > nums[i])
+        if (nums[left] < nums[i])
             swap(nums, indexes, ++pivot, i);
     swap(nums, indexes, left, pivot);
     qsort_(nums, indexes, left, pivot-1);
@@ -75,58 +75,55 @@ int* get_indexes(int size)
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
-    struct item **get_items(int *nums, int size);
     void qsort_(int *items, int *indexes, int left, int right);
     int binsearch(int* arr, int size, int target);
-    void freemem(struct item **items, int size);
     int* get_indexes(int size);
 
     int t;
-    int *res = malloc(sizeof (int)*2);
     int n1, n2;
-    int *indexes = get_indexes(numsSize);
-
-    qsort_(nums, indexes, 0, numsSize-1);
 
     int idx1 = 0;
     int idx2 = NOT;
 
-    for (; idx1 < numsSize && nums[idx1] < target; ++idx1)
-        ;
+    int *res = malloc(sizeof (int)*2);
+    int *indexes = get_indexes(numsSize);
 
-    --idx1;
+    qsort_(nums, indexes, 0, numsSize-1);
 
     do 
     {
-        n1 = nums[idx1--];
+        n1 = nums[idx1++];
         t = target - n1;
         if ((idx2 = binsearch(nums, numsSize, t)) == NOT
-                                    || (idx1 + 1) == idx2)
+                                    || (idx1 - 1) == idx2)
                 continue;
 
         n2 = nums[idx2];
     }
     while (target - (n1 + n2));
+    --idx1;
 
-    if (idx2 == NOT) 
+    if (!(target-(n1 + n2)))
+    {
+        int i1 = indexes[idx1];
+        int i2 = indexes[idx2];
+
+        res[0] = i1 < i2 ? i1 : i2;
+        res[1] = i1 > i2 ? i1 : i2;
+
+        *returnSize = 2;
+
+        free(indexes);
+
+        return res;
+    }
+    else 
     {
         free(indexes);
         return NULL;
     }
 
-    ++idx1;
 
-    int i1 = indexes[idx1];
-    int i2 = indexes[idx2];
-
-    res[0] = i1 < i2 ? i1 : i2;
-    res[1] = i1 > i2 ? i1 : i2;
-
-    *returnSize = 2;
-
-    free(indexes);
-
-    return res;
 }
 
 struct inp_data {
@@ -136,36 +133,41 @@ struct inp_data {
     int solution[2];
     int retSize;
 };
+#define TEST_LENGTH 1
+struct inp_data inputs[] = {
+  //{ { 2, 7, 11, 15 }, 4, 9, { 0, 1 }, 2 },
+  //{ { 3, 2, 4 }, 3, 6, { 1, 2 }, 2 },
+  //{ { 0, 4, 3, 0 }, 4, 0, { 0, 3 }, 2 },
+  //{ {}, 0, 0, {}, 0 },
+  //{ {-100, 99, 2, 0, 22, -1001, 500}, 7, -100, { 0,3 }, 2 },
+    { { -10, -1, -18, -19 }, 4, -19, { 1, 2 }, 2 }
+
+};
 
 int main(void)
 {
-    struct inp_data inputs[] = {
-        {
-            { 2, 7, 11, 15 }, 4, 9, {0, 1}, 2
-        },
-        {
-            { 3, 2, 4 }, 3, 6, {1, 2}, 2
-        }
-    };
 
     int *nums = NULL;
 
 #define TEST
 #ifdef TEST
-    for (long i = 0; i < 2; ++i)
+    for (long i = 0; i < TEST_LENGTH; ++i)
     {
         if ((nums = twoSum(inputs[i].arr, inputs[i].size,
                         inputs[i].target, &inputs[i].retSize)) != NULL) {
             for(int j = 0; j < inputs[i].size; ++j)
                 printf("%d ", inputs[i].arr[j]);
             printf("\ntarget: %d\n", inputs[i].target);
-            printf("solution: [%d %d]\n\n", nums[0], nums[1]);
+            printf("result:    [%d %d],\n", nums[0], nums[1]);
+            printf("soulution: [%d %d], %s\n\n", inputs[i].solution[0],
+                    inputs[i].solution[1],
+                    (nums[0] == inputs[i].solution[0] 
+                    && nums[1] == inputs[i].solution[1]) ? "PASSED" : "FAILED");
         }
 
     free(nums);
     }
     printf("Done...\n");
-    getchar();
 #else
     if ((nums = twoSum(input, input_length, target, &returnSize)) != NULL) {
         for (int i = 0; i < input_length; ++i)
