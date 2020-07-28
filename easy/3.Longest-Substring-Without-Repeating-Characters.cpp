86c12d2db58892e56f009eb8b6764a9ece171758
skillzor <skillzorwispa@gmail.com>
2020-07-28 16:09:59 +0700

    solved 3-rd problem

---
+++ easy/3.Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,57 @@
+//
+// Created by skillzor on 7/27/20.
+//
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    static int lengthOfLongestSubstring(string s) {
+        int result = 0;
+        string str;
+
+        for (char c : s) {
+            if (std::size_t found{str.find(c)}; found != std::string::npos) {
+                str.erase(str.begin(), str.begin() + (found+1));
+                str.push_back(c);
+            }
+            else {
+                str.push_back(c);
+                if (result < str.length())
+                    result = str.length();
+            }
+        }
+        return result;
+    }
+};
+
+Solution solution;
+int (*tested)(string s) = &Solution::lengthOfLongestSubstring;
+
+void test(string in, int out)
+{
+    static int count = 1;
+    int res;
+    cout << "test: " << count << " "
+         << (((res = (*tested)(in)) == out) ? "PASSED" : "FAILED") << " "
+         << "result: " << res << " "
+         << "solution: " << out
+         << "\n";
+    ++count;
+}
+
+int main() {
+    test("abcabcbb", 3);
+    test("bbbbb", 1);
+    test("pwwkew", 3);
+
+    solution.lengthOfLongestSubstring("abcabcbb");
+
+    return 0;
+}
