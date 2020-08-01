7c6fc6a9530c4ddf3bb90c3e36e98202bf4909d7
skillzor <skillzorwispa@gmail.com>
2020-08-01 13:17:50 +0700

    clear

--- easy/4.Median-of-Two-Sorted-Arrays.cpp
+++ easy/4.Median-of-Two-Sorted-Arrays.cpp
@@ -7,105 +7,39 @@
 using namespace std;
 
 class Solution {
-    int maxLeft(vector<int> v1, vector<int> v2, unsigned long i, unsigned long j) {
+    int maxLeft(vector<int> v, unsigned long i, unsigned long j) {
         --i, --j;
-        if (i >= 0 && j >=0 && i < v1.size() && j < v2.size()) {
-            return v1[i] > v2[j] ? v1[i] : v2[j];
+        if (i >= 0 && j >=0 && i < v.size() && j < v.size()) {
+            return v[i] > v[j] ? v[i] : v[j];
         }
-        else if (i >= 0 && i < v1.size()) {
-            return v1[i];
+        else if (i >= 0 && i < v.size()) {
+            return v[i];
         }
-        else if (j >= 0 && j < v2.size()) {
-            return v2[j];
+        else if (j >= 0 && j < v.size()) {
+            return v[j];
         }
         else
             return -9999999;
     }
-    int minRight(vector<int> v1, vector<int> v2, unsigned long i, unsigned long j) {
+    int minRight(vector<int> v, unsigned long i, unsigned long j) {
         ++i, ++j;
-        if (i >= 0 && j >=0 && i < v1.size() && j < v2.size()) {
-            return v1[i] < v2[j] ? v1[i] : v2[j];
+        if (i >= 0 && j >=0 && i < v.size() && j < v.size()) {
+            return v[i] < v[j] ? v[i] : v[j];
         }
-        else if (i >= 0 && i < v1.size()) {
-            return v1[i];
+        else if (i >= 0 && i < v.size()) {
+            return v[i];
         }
-        else if (j >= 0 && j < v2.size()) {
-            return v2[j];
+        else if (j >= 0 && j < v.size()) {
+            return v[j];
         }
         else
             return 9999999;
     }
-    int getJ(int i, int len) { return ((len-2*i) / 2) - 1; }
 public:
+    unsigned long len1;
+    unsigned long len2;
+
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> v1;
-        vector<int> v2;
-        unsigned long i = 0, j = 0;
-        unsigned long imin, imax, imid;
-        unsigned long totalLength = v1.size() + v2.size();
-        int maxL, minR;
-        int bigger;
-
-        if (nums1.size() && nums2.size()) {
-            v1 = { ((nums1.size() >= nums2.size() ? nums1 : nums2)) };
-            v2 = {((nums1.size() < nums2.size() ? nums1 : nums2)) };
-        }
-        else {
-            v1 = { (nums1.size() ? nums1 : nums2) };
-            v2 = { (nums1.size() ? nums1 : nums2) };
-        }
-        totalLength = v1.size() + v2.size();
-
-        if (!(totalLength % 2)) {
-            imin = (v1.size() - v2.size() ) / 2;
-            imax = totalLength / 2 - 1;
-        }
-        else {
-            imin = (v1.size() - v2.size() ) / 2;
-            imax = totalLength / 2;
-        }
-        while (imin <= imax) {
-
-            imid = (imin + imax) / 2;
-            i = imid;
-            j = getJ(i, totalLength);
-
-            if (i >= 0 && j >=0 && i < v1.size() && j < v2.size()) {
-                bigger = v1[i] > v2[j] ? v1[i] : v2[j];
-            }
-            else if (i >= 0 && i < v1.size())
-                bigger = v1[i];
-            else if (j >= 0 && j < v2.size())
-                bigger = v2[j];
-
-            if (totalLength % 2) {
-                if ((maxL = maxLeft(v1 , v2, i, j)) <= (minR = minRight(v1, v2, i, j))) {
-                    if (maxL <= bigger && bigger <= minR)
-                        return bigger;
-                }
-            }
-            else {
-                if ((maxL = maxLeft(v1 , v2, i, j)) <= (minR = minRight(v1, v2, i, j))
-                        && (maxL <= v1[i] && v1[i] <= minR)
-                        && (maxL <= v2[j] && v2[j] <= minR) )
-                            return (v1[i] + v2[j]) / 2.0;
-            }
-            if (bigger < minR ) {
-                if (bigger == v1[i])
-                    imax = imid - 1;
-                else
-                    imin = imid + 1;
-                continue;
-            }
-            else if (bigger > maxL) {
-                if (bigger == v2[j])
-                    imin = imid + 1;
-                else
-                    imax = imid - 1;
-                continue;
-            }
-        }
-        return 0.0;
     }
 };
 
@@ -130,16 +64,21 @@ void test(vector<int> vec1, vector<int> vec2, double out )
 }
 
 int main() {
-    test(vector<int>{ 1, 2 }, vector<int>{ -1, 3 }, 1.5);
-    test(vector<int>{ -2, -1}, vector<int>{3}, -1.0);
-    test(vector<int>{}, vector<int>{1}, 1.0);
+    test(vector<int>{ 4, 5, 6, 7, 8, 9}, vector<int>{ 1, 2, 3 }, 5.0);
     test(vector<int>{ 1, 2, 3, 4, 5, 6 }, vector<int>{ 25, 26, 27 }, 5.0);
-    test(vector<int>{ 4, 20, 32, 50, 55, 61 }, vector<int>{ 1, 15, 22, 30, 70 }, 30.0);
+    test(vector<int>{ 1, 2 }, vector<int>{ -1, 3 }, 1.5);
     test(vector<int>{ 1, 3 }, vector<int>{ 2 }, 2.0);
+    test(vector<int>{ 2, 3 }, vector<int>{ 1 }, 2.0);
+    test(vector<int>{ 1, 3 }, vector<int>{ 2 }, 2.0);
+    test(vector<int>{ 1, 3, 5, 7, 9 }, vector<int>{ 6, 8, 10 }, 6.5);
+
+    test(vector<int>{ 4, 20, 32, 50, 55, 61 }, vector<int>{ 1, 15, 22, 30, 70 }, 30.0);
+    test(vector<int>{ 1, 2 }, vector<int>{ 3 }, 2.0);
+    test(vector<int>{ -2, -1}, vector<int>{ 3 }, -1.0);
+    test(vector<int>{ }, vector<int>{ 1 }, 1.0);
     test(vector<int>{ 1, 2 }, vector<int>{ 3, 4 }, 2.5);
     test(vector<int>{ 1, 3, 5 }, vector<int>{ 2, 4, 6 }, 3.5);
     test(vector<int>{ 1, 3, 5, 7, 9 }, vector<int>{ 4, 6, 8 }, 5.5);
-    test(vector<int>{ 1, 3, 5, 7, 9 }, vector<int>{ 6, 8, 10 }, 6.5);
 
 
     return 0;
